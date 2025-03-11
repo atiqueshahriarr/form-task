a622b48158951629bac44b8fbd1e119422fa2127
atique-shahriar <satique06@gmail.com>
2025-03-11 11:03:59 +0600

    Create input field and select box

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import "./App.css";
 
 function App() {
-  return <div className="">Home</div>;
+  return (
+    <div className="">
+      <form action="">
+        <div className="flex gap-4">
+          <div>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              className=" w-56 border-2 px-4 py-2 rounded-lg outline-0"
+            />
+          </div>
+          <div>
+            <select
+              className=" w-30 border-2 px-4 py-2 rounded-lg outline-0"
+              name="gender"
+              id="gender">
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="others">Others</option>
+            </select>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
