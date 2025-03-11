40e58bb83060f6fd6eb9607b998f2247e6101b39
atique-shahriar <satique06@gmail.com>
2025-03-11 11:59:50 +0600

    Add button to add new field

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const [name, setName] = useState("");
   const [gender, setGender] = useState("");
 
+  const [field, setField] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
@@ -29,12 +31,19 @@ function App() {
     }
   };
 
+  const handleAddField = (e) => {
+    setField(field+1);
+  };
+
   return (
     <div className="">
-      <form
-        action=""
-        onSubmit={handleSubmit}>
-        <div className="flex gap-4">
+      <button onClick={handleAddField}>+</button>
+
+      {new Array(field).fill(null).map((_, index) => (
+        <form
+          action=""
+          onSubmit={handleSubmit}
+          className="flex gap-4">
           <div>
             <input
               type="text"
@@ -56,19 +65,56 @@ function App() {
             </select>
             {genderError ? <div>Select your gender</div> : <></>}
           </div>
+
+          <input
+            type="submit"
+            value="Submit"
+            className="bg-gray-400 px-3"
+          />
+        </form>
+      ))}
+
+      {/* <form
+        action=""
+        onSubmit={handleSubmit}
+        className="flex gap-4">
+        <div>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter your name"
+            className=" w-56 border-2 px-4 py-2 rounded-lg outline-0"
+          />
+          {nameError ? <div>Please enter your name</div> : <></>}
         </div>
+        <div>
+          <select
+            className=" w-56 border-2 px-4 py-2 rounded-lg outline-0"
+            name="gender"
+            id="gender">
+            <option>Select Option</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Others">Others</option>
+          </select>
+          {genderError ? <div>Select your gender</div> : <></>}
+        </div>
+
         <input
           type="submit"
           value="Submit"
+          className="bg-gray-400 px-3"
         />
-      </form>
+      </form> */}
       <div>
-        {
-          name && gender? <div>
+        {name && gender ? (
+          <div>
             <h3>Name: {name}</h3>
             <h3>Gender: {gender}</h3>
-          </div>:<></>
-        }
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
